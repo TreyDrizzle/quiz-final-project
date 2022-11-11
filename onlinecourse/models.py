2b26516bbe9ea3b0ff0604f1f35bf0c6545d0cbc
TreyDrizzle <davistyler.chad@gmail.com>
2022-11-11 10:04:30 -0500

    11/11

--- onlinecourse/models.py
+++ onlinecourse/models.py
@@ -1,7 +1,7 @@
 import sys
 from django.utils.timezone import now
 try:
-    from .models import Course, Enrollment, Question, Choice, Submission  
+    from django.db import models  
 
 except Exception:
     print("There was an error loading django modules. Do you have django installed?")
